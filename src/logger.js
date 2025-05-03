c9db019d8b3cd76b625be0bf9f739a03b266b194
Martin Gruber <mortlp121@gmail.com>
2025-05-03 18:02:27 +0200

    Logger

---
+++ src/logger.js
@@ -0,0 +1,20 @@
+const { createLogger, format, transports } = require('winston');
+const path = require('path');
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.printf(({ timestamp, level, message }) => {
+      return `[${timestamp}] [${level.toUpperCase()}]: ${message}`;
+    })
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({
+      filename: path.join(__dirname, '..', 'backup.log'),
+    }),
+  ],
+});
+
+module.exports = logger;
